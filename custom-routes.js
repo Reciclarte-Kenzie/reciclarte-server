b10df17a1218b5ec5de4a7d62889d6ca5545751b
Axionauta <daniln2209@gmail.com>
2023-01-02 21:34:13 -0300

    feat: add filtering by categorie

--- custom-routes.js
+++ custom-routes.js
@@ -3,14 +3,26 @@ const fileSystem = require("fs");
 const router = express.Router();
 
 router.get("/ideas/search", (request, response) => {
-  const searchedMaterials = request.query.materials instanceof Array ? request.query.materials : [request.query.materials];
-  const ideasData = (JSON.parse(fileSystem.readFileSync("db.json", "utf-8")).ideas);
+  const database = JSON.parse(fileSystem.readFileSync("db.json", "utf-8"));
+  let ideasData = database.ideas;
   
-  const ideasWithTheSpecifiedMaterial = ideasData.filter( idea => {
-    return searchedMaterials.every(searchedMaterial => idea.materials.find(ideaMaterial => ideaMaterial.toLowerCase() == searchedMaterial.toLowerCase()));
-  });
+  if(request.query.materials) {
+    ideasData = ideasData.filter( idea => {
+      const searchedMaterials = request.query.materials instanceof Array ? request.query.materials : [request.query.materials];
 
-  response.send(ideasWithTheSpecifiedMaterial);
+      return searchedMaterials.every(searchedMaterial => idea.materials.find(ideaMaterial => ideaMaterial.toLowerCase() == searchedMaterial.toLowerCase()));
+    });
+  }
+
+  if(request.query.categories) {
+    ideasData = ideasData.filter( idea => {
+      const searchedCategories = request.query.categories instanceof Array ? request.query.categories : [request.query.categories];
+  
+      return searchedCategories.every(searchedCategorie => idea.categories.find(ideaCategorie => ideaCategorie.toLowerCase() == searchedCategorie.toLowerCase()));
+    })
+  }
+
+  response.send(ideasData);
 });
 
 module.exports = router;
