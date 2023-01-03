051ca7b77cdf9d4ee61f0fae288520ee4aeea4f1
Axionauta <daniln2209@gmail.com>
2023-01-02 22:25:29 -0300

    feat: add filter by estimated cost

--- custom-routes.js
+++ custom-routes.js
@@ -2,24 +2,49 @@ const express = require("express");
 const fileSystem = require("fs");
 const router = express.Router();
 
-router.get("/ideas/search", (request, response) => {
+router.get("/ideas", (request, response) => {
   const database = JSON.parse(fileSystem.readFileSync("db.json", "utf-8"));
-  let ideasData = database.ideas;
-  
-  if(request.query.materials) {
-    ideasData = ideasData.filter( idea => {
-      const searchedMaterials = request.query.materials instanceof Array ? request.query.materials : [request.query.materials];
 
-      return searchedMaterials.every(searchedMaterial => idea.materials.find(ideaMaterial => ideaMaterial.toLowerCase() == searchedMaterial.toLowerCase()));
+  let ideasData = database.ideas;
+  let searchedMaterials = request.query.materials;
+  let searchedCategories = request.query.categories;
+  let difficultyLevel = request.query.difficulty_level;
+  let maximumCost = request.query.maximum_cost;
+
+  if (searchedMaterials) {
+    ideasData = ideasData.filter((idea) => {
+      searchedMaterials =
+        searchedMaterials instanceof Array
+          ? searchedMaterials
+          : [searchedMaterials];
+
+      return searchedMaterials.every((searchedMaterial) =>
+        idea.materials.find(
+          (ideaMaterial) =>
+            ideaMaterial.toLowerCase() == searchedMaterial.toLowerCase()
+        )
+      );
     });
   }
 
-  if(request.query.categories) {
-    ideasData = ideasData.filter( idea => {
-      const searchedCategories = request.query.categories instanceof Array ? request.query.categories : [request.query.categories];
-  
-      return searchedCategories.every(searchedCategorie => idea.categories.find(ideaCategorie => ideaCategorie.toLowerCase() == searchedCategorie.toLowerCase()));
-    })
+  if (searchedCategories) {
+    ideasData = ideasData.filter((idea) => {
+      searchedCategories =
+        searchedCategories instanceof Array
+          ? searchedCategories
+          : [searchedCategories];
+
+      return searchedCategories.every((searchedCategorie) =>
+        idea.categories.find(
+          (ideaCategorie) =>
+            ideaCategorie.toLowerCase() == searchedCategorie.toLowerCase()
+        )
+      );
+    });
+  }
+
+  if (maximumCost) {
+    ideasData = ideasData.filter((idea) => idea.estimated_cost <= maximumCost);
   }
 
   response.send(ideasData);
